3e44da76f34ae29124cf3d27946c962d5f395d4b
Alexander Voigt <ahgvoigt@gmail.com>
2021-12-06 18:54:16 +0100

    adding test for log1p implementations

--- Makefile
+++ Makefile
@@ -6,17 +6,12 @@ BBL := $(NAM).bbl
 TAR := $(NAM).tar.gz
 SRC := $(wildcard src/*.c)
 OBJ := $(SRC:.c=.o)
+TST := src/test_log1p.x
 
 all: $(PDF)
 
 arxiv: $(TAR)
 	@true
-
-test-arxiv: arxiv
-	-rm -rf paper
-	tar -xf paper.tar.gz
-	cd paper && pdflatex paper.tex && pdflatex paper.tex
-	-rm -rf paper
 
 clean:
 	latexmk -c
@@ -28,6 +23,15 @@ distclean: clean
 compile: $(OBJ)
 	@true
 
+test: $(TST)
+	$<
+
+test-arxiv: arxiv
+	-rm -rf paper
+	tar -xf paper.tar.gz
+	cd paper && pdflatex paper.tex && pdflatex paper.tex
+	-rm -rf paper
+
 $(PDF): $(TEX) $(BIB)
 	latexmk $(TEX)
 
@@ -36,3 +40,6 @@ $(TAR): $(TEX) $(BIB) $(BBL) $(SRC) | all
 
 %.o: %.c
 	$(CC) -O2 -c $< -o $@
+
+%.x: $(OBJ)
+	$(CC) -O2 $^ -o $@ -lm
---
+++ src/test_log1p.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <math.h>
+
+unsigned failed = 0;
+unsigned passed = 0;
+
+double log1p_naive(double);
+double log1p_horner(double);
+double log1p_estrin(double);
+double log1p_pade(double);
+double log1p_mixed(double);
+
+struct Fn {
+   double (*f)(double);
+   const char* name;
+};
+
+const struct Fn functions[] = {
+   { log1p_naive , "log1p_naive"  },
+   { log1p_horner, "log1p_horner" },
+   { log1p_estrin, "log1p_estrin" },
+   { log1p_pade  , "log1p_pade"   },
+   { log1p_mixed , "log1p_mixed"  },
+};
+
+
+const double values[] = {
+   0, 0.1
+};
+
+
+#define CHECK_CLOSE(val,a,b,eps,name) do {                              \
+      const double aa = fabs(a);                                        \
+      const double ab = fabs(b);                                        \
+      const double mx = aa > ab ? aa : ab;                              \
+      const double ae = fabs(eps);                                      \
+      if (fabs(aa - ab) < ae*(1 + mx)) {                                \
+         passed++;                                                      \
+      } else {                                                          \
+         printf("Error in %s(%g): %g ~!~ %g with eps = %g\n", name, val, aa, ab, ae); \
+         failed++;                                                      \
+      }                                                                 \
+   } while (0);
+
+
+int main() {
+   const double eps = 1e-6;
+
+   for (int fi = 0; fi < sizeof(functions)/sizeof(functions[0]); ++fi) {
+      for (int v = 0; v < sizeof(values)/sizeof(values[0]); ++v) {
+         const double (*f)(double) = functions[fi].f;
+         const double val = values[v];
+         CHECK_CLOSE(val, f(val), log1p(val), eps, functions[fi].name);
+      }
+   }
+
+   printf("Tests passed: %u/%u\n", passed, passed + failed);
+
+   return failed > 0;
+}
