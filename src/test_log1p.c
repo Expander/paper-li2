f13fd1094f7502c5639710ee090c7921f1e4197a
Alexander Voigt <ahgvoigt@gmail.com>
2021-12-07 08:35:49 +0100

    move function declarations into a header

--- src/test_log1p.c
+++ src/test_log1p.c
@@ -1,27 +1,9 @@
 #include <stdio.h>
 #include <math.h>
+#include "log1p.h"
 
 unsigned failed = 0;
 unsigned passed = 0;
-
-double log1p_naive(double);
-double log1p_horner(double);
-double log1p_estrin(double);
-double log1p_pade(double);
-double log1p_mixed(double);
-
-struct Fn {
-   double (*f)(double);
-   const char* name;
-};
-
-const struct Fn functions[] = {
-   { log1p_naive , "log1p_naive"  },
-   { log1p_horner, "log1p_horner" },
-   { log1p_estrin, "log1p_estrin" },
-   { log1p_pade  , "log1p_pade"   },
-   { log1p_mixed , "log1p_mixed"  },
-};
 
 
 const double values[] = {
