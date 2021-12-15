ca9f3a6e1166abd0ddac5fa72ec79d0491f8e7f9
Alexander Voigt <ahgvoigt@gmail.com>
2021-12-15 08:41:17 +0100

    avoid fp division

--- src/log1p_estrin.c
+++ src/log1p_estrin.c
@@ -1,6 +1,6 @@
 double log1p_estrin(double x) {
    const double y = x*x;
    const double z = y*y;
-   return x + (x/3 - 0.5)*y
-      + (y*(-1./6 + x/7) + x/5 - 0.25)*z;
+   return x + (1./3*x - 1./2)*y
+      + (y*(-1./6 + 1./7*x) + x/5 - 1./4)*z;
 }
--- src/log1p_naive.c
+++ src/log1p_naive.c
@@ -1,5 +1,5 @@
 double log1p_naive(double x) {
-   return x - x*x/2 + x*x*x/3 - x*x*x*x/4
-      + x*x*x*x*x/5 - x*x*x*x*x*x/6
-      + x*x*x*x*x*x*x/7;
+   return x - 1./2*x*x + 1./3*x*x*x
+      - 1./4*x*x*x*x + 1./5*x*x*x*x*x
+      - 1./6*x*x*x*x*x*x + 1./7*x*x*x*x*x*x*x;
 }
