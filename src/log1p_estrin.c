fd20022f1d65b5440edd1d9105884b8a28a6f1d1
Alexander Voigt <ahgvoigt@gmail.com>
2021-12-27 09:52:41 +0100

    fix typos

--- src/log1p_estrin.c
+++ src/log1p_estrin.c
@@ -1,6 +1,6 @@
 double log1p_estrin(double x) {
    const double y = x*x;
    const double z = y*y;
-   return x + (1./3*x - 1./2)*y +
-      (y*(-1./6 + 1./7*x) + 1./5*x - 1./4)*z;
+   return x + y*(1./3*x - 1./2) +
+      z*(1./5*x - 1./4 + y*(-1./6 + 1./7*x));
 }
